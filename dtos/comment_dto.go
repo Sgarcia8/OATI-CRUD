c77e894bb27804c2428ee6d546f1a3eb44658b3f
sjgarciat <sgarcia@verticcal.com>
2026-06-18 16:26:34 -0500

    fix: ejemplos en swagger

--- dtos/comment_dto.go
+++ dtos/comment_dto.go
@@ -12,10 +12,10 @@ type UpdateCommentRequest struct {
 
 type CommentResponse struct {
 	Id         int       `json:"id"`
-	Content    string    `json:"content"`
-	TutorialId int       `json:"tutorial_id"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	Content    string    `json:"content" example:"Comentario de prueba" description:"Contenido del comentario"`
+	TutorialId int       `json:"tutorial_id" example:"1" description:"ID del tutorial"`
+	CreatedAt  time.Time `json:"created_at" example:"2026-06-18T00:00:00Z" description:"Fecha de creación del comentario"`
+	UpdatedAt  time.Time `json:"updated_at" example:"2026-06-18T00:00:00Z" description:"Fecha de actualización del comentario"`
 }
 
 type CommentListResponse struct {
--- dtos/tutorial_dto.go
+++ dtos/tutorial_dto.go
@@ -3,24 +3,24 @@ package dtos
 import "time"
 
 type CreateTutorialRequest struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	PublishedAt string `json:"published_at"`
+	Title       string `json:"title" example:"Tutorial de prueba" description:"Título del tutorial"`
+	Description string `json:"description" example:"Descripción del tutorial" description:"Descripción del tutorial"`
+	PublishedAt string `json:"published_at" example:"2026-06-18T00:00:00Z" description:"Fecha de publicación del tutorial"`
 }
 
 type UpdateTutorialRequest struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	PublishedAt string `json:"published_at"`
+	Title       string `json:"title" example:"Tutorial de prueba" description:"Título del tutorial"`
+	Description string `json:"description" example:"Descripción del tutorial" description:"Descripción del tutorial"`
+	PublishedAt string `json:"published_at" example:"2026-06-18T00:00:00Z" description:"Fecha de publicación del tutorial"`
 }
 
 type TutorialResponse struct {
 	Id          int       `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	PublishedAt time.Time `json:"published_at"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	Title       string    `json:"title" example:"Tutorial de prueba" description:"Título del tutorial"`
+	Description string    `json:"description" example:"Descripción del tutorial" description:"Descripción del tutorial"`
+	PublishedAt time.Time `json:"published_at" example:"2026-06-18T00:00:00Z" description:"Fecha de publicación del tutorial"`
+	CreatedAt   time.Time `json:"created_at" example:"2026-06-18T00:00:00Z" description:"Fecha de creación del tutorial"`
+	UpdatedAt   time.Time `json:"updated_at" example:"2026-06-18T00:00:00Z" description:"Fecha de actualización del tutorial"`
 }
 
 type TutorialListResponse struct {
@@ -30,10 +30,10 @@ type TutorialListResponse struct {
 
 type TutorialDetailResponse struct {
 	Id          int                `json:"id"`
-	Title       string             `json:"title"`
-	Description string             `json:"description"`
-	PublishedAt time.Time          `json:"published_at"`
-	CreatedAt   time.Time          `json:"created_at"`
-	UpdatedAt   time.Time          `json:"updated_at"`
-	Comments    []*CommentResponse `json:"comments"`
+	Title       string             `json:"title" example:"Tutorial de prueba" description:"Título del tutorial"`
+	Description string             `json:"description" example:"Descripción del tutorial" description:"Descripción del tutorial"`
+	PublishedAt time.Time          `json:"published_at" example:"2026-06-18T00:00:00Z" description:"Fecha de publicación del tutorial"`
+	CreatedAt   time.Time          `json:"created_at" example:"2026-06-18T00:00:00Z" description:"Fecha de creación del tutorial"`
+	UpdatedAt   time.Time          `json:"updated_at" example:"2026-06-18T00:00:00Z" description:"Fecha de actualización del tutorial"`
+	Comments    []*CommentResponse `json:"comments" example:"[{'id': 1, 'content': 'Comentario de prueba', 'tutorial_id': 1, 'created_at': '2026-06-18T00:00:00Z', 'updated_at': '2026-06-18T00:00:00Z'}]"`
 }
